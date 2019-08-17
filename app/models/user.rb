f527464cdfca4847ac8ce0360dd5a34aefa3f285
si0005hp <si0005hp@gmail.com>
2019-08-17 18:31:01 +0900

    Create Note model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 # User
 class User < ApplicationRecord
+  has_many :notes, dependent: :destroy
+
   def self.from_token_payload(payload)
     find_by(sub: payload['sub']) || create!(sub: payload['sub'])
   end
